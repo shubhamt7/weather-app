151ad1ba4063770321212e653907ccdd6f20969c
shubhamthind7@gmail.com <shubhamthind7@gmail.com>
2019-12-18 01:04:34 +0530

    Modified forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request= require('request')
 
 
 const forecast=(lat,long,callback)=>{
-    const url="https://api.darksky.net/forecast/14b0c5aa4a381a623d31665628a4b5b7/"+lat+","+long
+    const url="https://api.darksky.net/forecast/14b0c5aa4a381a623d31665628a4b5b7/"+lat+","+long+"?units=si"
 
     request({url, json: true},(error, {body} )=>{
         if(error){
@@ -10,7 +10,11 @@ const forecast=(lat,long,callback)=>{
         }else if(body.error){
             callback("Unable to find location", undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary)
+            const summary= body.daily.data[0].summary
+            const temp= body.hourly.data[0].temperature
+            const rainProbability= body.daily.data[0].precipProbability*100
+            const forecastResult= summary + " It is currently "+ temp +" degrees temperature out there. There is "+ rainProbability+"% chance of rain."
+            callback(undefined, forecastResult)
         }
     })
 
